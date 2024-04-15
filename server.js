23bcdad5ab7b27a506ea9ea70facb8af01e79888
unknown <frankortiz5249@gmail.com>
2024-04-15 12:54:55 -0700

    completed day 5 and everything before that

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require ('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUALIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate()
+    console.log('Connected with Sequelize at ${process.env.PG_URI}')
+} catch(err) {
+    console.log('Unable to connect to PG: ${err}')
+}
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +25,18 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// Band Root
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+// Stage Route
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
+
+// Event Route
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
