5b671c00d7d79f059d23c727545670f724fb020e
unknown <frankortiz5249@gmail.com>
2024-04-16 17:23:58 -0700

    add association defs on models

--- models/event.js
+++ models/event.js
@@ -4,13 +4,25 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({ Stage, StageEvent, MeetGreet, SetTime }) {
+      // stages
+      Event.belongsToMany(Stage, {
+        foreignKey: "event_id",
+        as: "stages",
+        through: StageEvent
+      })
+
+       // meet and greets 
+       Event.hasMany(MeetGreet, {
+        foreignKey: "event_id",
+        as: "meet_greets"
+      })
+
+      // set times 
+      Event.hasMany(SetTime, {
+        foreignKey: "event_id",
+        as: "set_times"
+      })
     }
   }
   Event.init({
@@ -27,14 +39,14 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.DATE,
       allowNull: false
     },
-    //start_time: {
-      //type: DataTypes.TIME,
-      //allowNull: false
-    //},
-    //end_time: {
-      //type: DataTypes.TIME,
-      //allowNull: false
-    //},
+    start_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    end_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
   }, {
     sequelize,
     modelName: 'Event',
